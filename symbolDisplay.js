e7fcc7be30ddbd0580afc6cbc0d97a147a11835d
luka0204 <Luka9609@gmail.com>
2024-09-14 19:57:53 +0300

    added main function ( matching hiragana and romaji spelling )

--- symbolDisplay.js
+++ symbolDisplay.js
@@ -1,25 +1,40 @@
+// symbolDisplay.js
 import { hiraganaList } from "./symbolList.js";
+
 
 console.log('JavaScript file loaded');
 
+
 console.log('hiraganaList:', hiraganaList);
+
 
 const kanaBox = document.querySelector('.kana-box');
 console.log('kanaBox:', kanaBox);
 
+
 const answerBox = document.querySelector('.answer-box');
 console.log('answerBox:', answerBox);
 
-setTimeout(() => {
-    const randomKey = Object.keys(hiraganaList)[Math.floor(Math.random() * Object.keys(hiraganaList).length)];
-    const randomKana = hiraganaList[randomKey];
-    console.log('randomKana:', randomKana);
-  
-    kanaBox.textContent = randomKana;
-  
-    // Get the Latin character corresponding to the Kana letter
-    const latinCharacter = Object.keys(hiraganaList).find(key => hiraganaList[key] === randomKana);
-    console.log('latinCharacter:', latinCharacter);
-  
-    answerBox.textContent = latinCharacter;
-  }, 1000);
+
+let currentKana = null;
+
+
+function showNextKana() {
+  const randomKey = Object.keys(hiraganaList)[Math.floor(Math.random() * Object.keys(hiraganaList).length)];
+  currentKana = hiraganaList[randomKey];
+  kanaBox.textContent = currentKana;
+  answerBox.value = ''; // Clear the input field
+}
+
+
+showNextKana();
+
+
+answerBox.addEventListener('input', () => {
+  const userAnswer = answerBox.value;
+  const correctAnswer = Object.keys(hiraganaList).find(key => hiraganaList[key] === currentKana);
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    showNextKana();
+  }
+});
